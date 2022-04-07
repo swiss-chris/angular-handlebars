179642353685be5a147fadd3b084a1f70cd10d1c
swiss-chris <chris.dickinson@web.de>
2022-04-07 17:37:32 +0300

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MyContainerComponent } from './components/my-container/my-container.component';
 import { SafeHtmlPipe } from './components/safe-html-pipe.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    MyContainerComponent,
-    SafeHtmlPipe,
-  ],
+  declarations: [AppComponent, MyContainerComponent, SafeHtmlPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
