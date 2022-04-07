19c988dcac395b9a38d4941a7dd81c69696af871
swiss-chris <chris.dickinson@web.de>
2022-04-07 16:32:42 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -17,7 +17,7 @@ export class AppComponent {
           color: green;
         }
       </style>
-      
+
       <div>
         <a href='https://www.medium.com'>This is a working link</a>
         <div class='green'>
