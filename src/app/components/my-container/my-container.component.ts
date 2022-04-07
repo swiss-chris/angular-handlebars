97252e594a8cc4645c41fc70284876a21a2a4a67
swiss-chris <chris.dickinson@web.de>
2022-04-07 12:10:05 +0300

    4 files modified

--- src/app/components/my-container/my-container.component.ts
+++ src/app/components/my-container/my-container.component.ts
@@ -5,8 +5,13 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './my-container.component.html',
   styleUrls: ['./my-container.component.css'],
 })
-export class MyContainerComponent {
+export class MyContainerComponent implements OnInit {
   constructor() {}
 
   @Input() data: { name: string; occupation: string };
+  @Input() template: { html: string };
+
+  ngOnInit() {
+    console.log(this.template.html);
+  }
 }
