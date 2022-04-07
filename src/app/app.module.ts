3f1b29590b1921f1f2476422f4a4568bbb6d4e35
swiss-chris <chris.dickinson@web.de>
2022-04-07 15:54:57 +0300

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MyContainerComponent } from './components/my-container/my-container.component';
+import { SafeHtmlPipe } from './components/safe-html-pipe.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, MyContainerComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MyContainerComponent,
+    SafeHtmlPipe,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
